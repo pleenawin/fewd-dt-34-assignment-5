c5a8f653b67315c4d4a4572962191b1868dc00f1
Paulina <pleena.win@gmail.com>
2018-03-22 19:51:35 -0700

    changed .css to .attr

--- starter_code/js/index.js
+++ starter_code/js/index.js
@@ -4,19 +4,19 @@ $(document).ready(function() { // wait for page to load
     	e.preventDefault(); // Prevent form submission
     	var city = $("#city-type").val().toLowerCase(); // lowercase inputs
     	if (city == 'new york' || city == 'new york city' || city == 'nyc') {
-            $("body").attr("src", "nyc.jpg");
+            $("body").attr("class", 'nyc');
             } // change to ny
         else if (city == 'san francisco' || city == 'sf' || city == 'bay area') {
-            $("body").css("background-image", "url('images/sf.jpg')");
+            $("body").attr("class", 'sf');
             } // change to other cities if not ny
         else if (city == 'los angeles' || city == 'la' || city == 'lax') {
-            $("body").css("background-image", "url('images/la.jpg')");
+            $("body").attr("class", 'la');
             }
         else if (city == 'austin' || city == 'atx') {
-            $("body").css("background-image", "url('images/austin.jpg')");
+            $("body").attr("class", 'austin');
             }
         else if (city == 'sydney' || city == 'syd') {
-            $("body").css("background-image", "url('images/sydney.jpg')");
+            $("body").attr("class", 'sydney');
             }
         else {
             $("body").css("background-image", ""); // or go back to default when input doesn't match any if statements
